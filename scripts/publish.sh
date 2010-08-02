75a8f6a54bcbfa9eb0aeddf88ed4c0a62d609f31
Michel Alexandre Salim <michel.salim@informatik.uni-erlangen.de>
2010-08-02 15:54:03 +0200

    Push only the interface.xsl in the year-specific directories

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-tar cf - dist/*.tar.* *.xsl 201*/*.{gpg,xml} images | \
+tar cf - dist/*.tar.* 201*/*.{gpg,x{ml,sl}} images | \
   ssh forge-0install@faui2o2b.informatik.uni-erlangen.de \
     'cd interfaces && tar xf -'
 
