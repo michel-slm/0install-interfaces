217185079eade4ea32404cb88c27bf5ba9de8fcb
Michel Alexandre Salim <michel.salim@informatik.uni-erlangen.de>
2010-08-02 16:12:23 +0200

    tomcat-controller: now put temporary files under /var/tmp/${USER}-tomcat

---
+++ dist/tomcat-controller-0.1.2/bin/tomcat.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# The directory where bin/tomcat.sh is located
+if [ -z "$CATALINA_HOME" ]; then
+    # assume that script is put in Tomcat's bin directory
+    CATALINA_HOME="$(cd $(dirname $0)/.. && pwd)"
+fi
+
+if [ -z "$1" ]; then
+    CMD=help
+else
+    CMD=$1
+fi
+
+# first, initialize the local user's .config
+[ -d "${HOME}/.config/tomcat" ] || (
+    mkdir -p ${HOME}/.config/tomcat
+    cd ${HOME}/.config/tomcat/
+    (cd ${CATALINA_HOME} && tar cf - conf logs) | tar xf -
+    chmod -R u=rwX *
+    mkdir webapps
+    ln -s ${CATALINA_HOME}/webapps/* webapps/
+)
+
+[ -d "/var/tmp/${USER}-tomcat" ] || mkdir "/var/tmp/${USER}-tomcat"
+
+export CATALINA_BASE="${HOME}/.config/tomcat"
+export CATALINA_TMPDIR="/var/tmp/${USER}-tomcat"
+
+case "$CMD" in
+    help)
+	echo "Provide one of the following options:"
+	echo "help    -- display this help"
+	echo "start   -- start Tomcat server"
+	echo "stop    -- stop the server"
+	echo "restart -- restart the server"
+	;;
+    start)
+	${CATALINA_HOME}/bin/startup.sh
+	;;
+    stop)
+	${CATALINA_HOME}/bin/shutdown.sh
+	;;
+    restart)
+	$0 stop; sleep 5; $0 start
+esac
